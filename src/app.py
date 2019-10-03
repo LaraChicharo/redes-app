7603cb89e954024a6f1cd62d835e228e43cb11d4
Cesar Lara <larachicharo@protonmail.com>
2019-10-03 18:59:05 -0500

    Using SQLAlchemy with an in-memory database.

---
+++ src/app.py
@@ -0,0 +1,81 @@
+from os import environ
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+POSTGRES_USER = environ['POSTGRES_USER']
+POSTGRES_PW = environ['POSTGRES_PW']
+POSTGRES_URL = environ['POSTGRES_URL']
+POSTGRES_DB = environ['POSTGRES_DB']
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=POSTGRES_USER,
+    pw=POSTGRES_PW,
+    url=POSTGRES_URL,
+    db=POSTGRES_DB
+)
+
+
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=False, nullable=False)
+    password = db.Column(db.String(256), index=False, nullable=False)
+
+db.create_all()
+
+
+def create_user(username, password):
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+
+def check_user_pass(username, password):
+    q = User.query.filter(
+        User.username == username and User.password == password
+    )
+    if q.first():
+       return True
+    else:
+        return False
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_user_pass(username, password):
+            return 'successful login'
+        else:
+            return 'combination of username/password doesn\'t exists'
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        comp_password = request.form['passwordc']
+        if password != comp_password:
+            return 'Passwords don\'t match'
+        else:
+            create_user(username, password)
+            return 'user created successfully'
+
